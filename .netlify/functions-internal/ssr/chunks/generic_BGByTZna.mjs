c82aacb529a30d3b4dd54d2e3ddddb9fa15bb98e
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2024-02-06 15:51:56 +0900

    feat: gitignore

--- .netlify/functions-internal/ssr/chunks/generic_BGByTZna.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_xVZy1J1L.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_TFlroxe1.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_-8E38Jsf.mjs');
-
-export { page };
