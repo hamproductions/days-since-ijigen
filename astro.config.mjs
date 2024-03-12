8aa2ce0548a6e6b6fe1e5220eab11d76d702d413
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2024-03-12 15:49:58 +0900

    feat: add gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import partytown from "@astrojs/partytown";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: netlify({
     edgeMiddleware: true,
-
-  })
+  }),
 });
