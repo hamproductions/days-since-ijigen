d1c3bb16e3c0ebb65184bc65b02ef940b8762f67
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2024-05-21 02:22:45 +0900

    feat: timezone format

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -1,5 +1,14 @@
-import { fromZonedTime } from "date-fns-tz";
+import { formatInTimeZone, toZonedTime } from "date-fns-tz";
+import { ja } from "date-fns/locale/ja";
 
 export const getDisplayDate = () => {
-  return fromZonedTime(new Date(), "Asia/Tokyo");
+  return toZonedTime(new Date(), "Asia/Tokyo");
 };
+
+export const formatDate = () => {
+  return formatInTimeZone(getDisplayDate(), "Asia/Tokyo", "yyyy年MM月dd日(E)", {
+    locale: ja,
+  });
+};
+
+// ${day.getFullYear()}年${day.getMonth() + 1}月${day.getDate()}日(${["日", "月", "火", "水", "木", "金", "土"][day.getDay()]}) ${event.time}
